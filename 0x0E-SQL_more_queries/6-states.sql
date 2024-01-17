6485fd3182e94df757b27dc7a1cef90dd24643f7
Anwar011 <elmouharibi12@gmail.com>
2024-01-17 01:16:26 -0800

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
--- ID?? is a Unique ID 
-CREATE TABLE IF NOT EXISTS unique_id(
-    id INT DEFAULT 1,
-    name VARCHAR(256),
-    UNIQUE(id)
-)
+-- Creates the table hbtn_0d_usa with table states.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
+);
